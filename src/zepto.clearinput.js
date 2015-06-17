6df999f66228c53c5afedb28a79b695aa941302f
catcto <catcto.com@gmail.com>
2015-06-17 22:25:50 +0800

    1.0.1

---
+++ src/zepto.clearinput.js
@@ -0,0 +1,34 @@
+/*
+ * zepto.clearinput
+ * 
+ */
+(function ($) {
+    $.fn.clearInput = function (options) {
+        var settings = $.extend({
+            'focus': true
+        }, options);
+        return this.each(function () {
+            var $this = $(this);
+            var $clear = $(settings.clear);
+            $clear.click(function () {
+                $this.val('').change();
+                $clear.hide();
+                if (settings.focus) {
+                    $this.focus();
+                }
+            });
+            function trigger() {
+                if ($this.val().length > 0) {
+                    $clear.show();
+                } else {
+                    $clear.hide();
+                }
+            }
+
+            $this.on('keyup keydown change focus', function () {
+                trigger();
+            });
+            trigger();
+        });
+    };
+})(Zepto);
